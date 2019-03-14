c98be8c3410b3cf5b0469f95b6221d23696496a5
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2019-03-14 22:51:13 +0000

    stack chart created

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ queue()
             ward: row.ED_Ward,
             tot_population: +row.TOTPOP,
             male_population: +row.MALE,
-            female_population: + row.FEMALE,
-            age_014: + row.AGE014T,
+            female_population: +row.FEMALE,
+            age_014: +row.AGE014T,
             age_1524: +row.AGE1524T,
             age_2544: +row.AGE2544T,
             age_4564: +row.AGE4564,
@@ -19,6 +19,7 @@ queue()
         var ndx = crossfilter(data);
         show_country_chart(ndx);
         show_county_chart(ndx);
+        show_stack_chart(ndx);
       
         dc.renderAll();
     }
@@ -41,8 +42,8 @@ function show_country_chart(ndx) {
 }
 
 function show_county_chart(ndx) {
-    var county_dim = ndx.dimension(dc.pluck('county'));
-    var population = county_dim.group().reduceSum(dc.pluck('tot_population'));
+    let county_dim = ndx.dimension(dc.pluck('county'));
+    let population = county_dim.group().reduceSum(dc.pluck('tot_population'));
 
     dc.pieChart('#county_chart')
         .width(150)
@@ -51,6 +52,38 @@ function show_county_chart(ndx) {
         .transitionDuration(500)
         .dimension(county_dim)
         .group(population)
-        
-        
+}
+
+function show_stack_chart(ndx){
+    let county_dim = ndx.dimension(dc.pluck('county'));
+    let age_one = county_dim.group().reduceSum(dc.pluck('age_014'));
+    let age_two = county_dim.group().reduceSum(dc.pluck('age_1524'));
+    let age_three = county_dim.group().reduceSum(dc.pluck('age_2544'));
+    let age_four = county_dim.group().reduceSum(dc.pluck('age_4564'));
+    let age_five = county_dim.group().reduceSum(dc.pluck('age_65plus'));
+
+    dc.barChart('#stack_chart')
+    .width(1000)
+    .height(350)
+    .dimension(county_dim)
+    .group(age_one,'0 to 14')
+    .stack(age_two,'14 to 24')
+    .stack(age_three,'25 to 44')
+    .stack(age_four,'45 to 64')
+    .stack(age_five,'64 plus')
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .margins({
+        top: 30,
+        left: 70,
+        bottom: 70,
+        right: 20
+    })
+    .legend(dc.legend().x(720).y(20).itemHeight(15).gap(5))
+    .useViewBoxResizing(true)
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel('Counties')
+    .yAxisLabel('Population')
+    .yAxis().ticks(7);
 }
